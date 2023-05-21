56d410ce14339152b24e9374d55d02d64115f3d4
Liliana Franco <lilimafra@hotmail.com>
2023-05-21 17:23:36 -0500

    Prueba

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,47 +1,52 @@
-import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import BasicLightbox from 'basiclightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
 console.log(galleryItems);
 
-// Construccion de la Galeria
-const instance = simpleLightbox.create(
-  `<li class= "gallery__item">
-        <a class= "gallery__link" href="${item.original}">
-            <img class= "gallery__image"
+// Construcción de la Galeria
+const galleryHTML = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image"
             src="${item.preview}" 
             data-source="${item.original}" 
             alt="${item.description}"/>
         </a>
-        </li>`
-);
-instance.show();
+    </li>`
+  )
+  .join('');
 
-//Insertar la galeria en el html
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('afterbegin', galleryHTML);
 
-galleryContainer.insertAdjacentHTML('afterbegin', builderGallery);
-
-//Evita que las imagenes de descarguen al hacer click
+// Evita que las imágenes se descarguen al hacer clic
 galleryContainer.addEventListener('click', event => event.preventDefault());
 
 // Ventana modal
 galleryContainer.addEventListener('click', clickOnImage);
 
-function clickOnImage(ImageAction) {
-  if (ImageAction.target.nodeName !== 'IMG') {
+function clickOnImage(event) {
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
 
-  const instance = basicLightbox.create(
-    `<img src="${ImageAction.target.dataset.source}" width="400" height="200">`
+  const instance = BasicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="400" height="200">`
   );
   instance.show();
 
-  // Evento tecla esc
-  galleryContainer.addEventListener('keydown', ImageAction => {
-    if (ImageAction.code === 'Escape') {
+  // Evento tecla Esc
+  const closeOnEscape = keyEvent => {
+    if (keyEvent.code === 'Escape') {
       instance.close();
+      galleryContainer.removeEventListener('keydown', closeOnEscape);
     }
-  });
+  };
+
+  galleryContainer.addEventListener('keydown', closeOnEscape);
 }
